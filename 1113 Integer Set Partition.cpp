e9cdf4fbd593bb2ca32d42dfa5efa6348a9d4348
molepi40 <molepi40@gmail.com>
2024-03-15 12:22:41 +0800

    re

--- 1113 Integer Set Partition.cpp
+++ 1113 Integer Set Partition.cpp
@@ -1,25 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void findKthNum(int* num, int s, int e, int k) {
+    if (s == e) {
+        return;
+    } else if (s > e) {
+        return;
+    }
+    int t = num[s], l = s, r = e;
+    while (l < r) {
+        while (l < r && num[r] > t) r--;
+        if (l < r) num[l++] = num[r];
+        while (l < r && num[l] < t) l++;
+        if (l < r) num[r--] = num[l];
+    }
+    num[l] = t;
+    if ((l - s + 1) == k) {
+        return;
+    } else if ((l - s + 1) < k) {
+        findKthNum(num, l + 1, e, k - (l - s + 1));
+    } else {
+        findKthNum(num, s, l - 1, k);
+    }
+}
+
 int main() {
-    int n, i;
+    int n;
     scanf("%d", &n);
     int num[n];
-    for (i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         scanf("%d", &num[i]);
     }
-    sort(num, num + n);
-    int n1 = n / 2, n2 = n - n1, sum1 = 0, sum2 = 0, ans;
-    i = 0;
+    int n1 = n / 2;
+    int n2 = n - n2;
+    findKthNum(num, 0, n-1, n1);
+    int sum1 = 0, sum2 = 0, i = 0;
     for (; i < n1; ++i) {
         sum1 += num[i];
     }
     for (; i < n; ++i) {
         sum2 += num[i];
     }
-    ans = abs(sum1 - sum2);
+    int ans = abs(sum1 - sum2);
     if (n2 > n1) {
         ans = max(ans, abs(sum1 - sum2 + 2 * num[n1]));
     }
-    printf("%d %d", n2 - n1, ans);
+    printf("%d %d", n - 2 * n1, ans);
 }
